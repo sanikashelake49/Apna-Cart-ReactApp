ae8daecfe009cb8c6193e784a1b2a117c4621e18
sanikashelake49 <shelakesanika49@gmail.com>
2026-07-02 23:52:09 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Navbar extends React.Component {
     state = {  } 
-    render() { 
+    render() {
         return (<>
            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
