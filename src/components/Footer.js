d815a6d5208e68614a0be241cd65cfbca7a773d6
sanikashelake49 <shelakesanika49@gmail.com>
2026-08-18 22:47:15 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer(props) {
     <div className="row fixed-bottom">
       <button
         className="btn btn-danger col-2"
-        onClick={() => {
+        onClick={() => { 
           props.resetQuantity();
         }}
       >
