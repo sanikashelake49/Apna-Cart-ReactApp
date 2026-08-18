697d160cc18d83027f4038e1c67cd844991b544b
sanikashelake49 <shelakesanika49@gmail.com>
2026-08-18 22:47:25 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer(props) {
         className="btn btn-danger col-2"
         onClick={() => { 
           props.resetQuantity();
-        }}
+        }} 
       >
         Reset
       </button>
